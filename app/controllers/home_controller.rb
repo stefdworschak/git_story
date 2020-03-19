13ada3f371bf69b28a1a4d1f882d11b20c0270d8
stefdworschak <stef.dworschak@gmail.com>
2020-03-19 22:33:49 +0000

    Adding logic to call Github API and display info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,30 @@
+require 'net/http'
+require 'json'
 class HomeController < ApplicationController
   def index
+    @query = params[:query]
+
+    if not @query.to_s.empty?
+        if params[:type] == 'dir'
+            puts @query
+            @response = Net::HTTP.get(URI.parse(@query))
+            puts @response
+        else  
+            @query = @query.downcase.tr(" ", "_")
+            @test = params[:query].split('')
+            if @test[@test.length() - 1] != '/'
+                @query.concat('/')
+            end
+            @response = Net::HTTP.get(URI.parse('https://api.github.com/repos/' + @query + 'contents'))
+        end        
+        @jsonContent = JSON.parse(@response)
+        if @jsonContent.kind_of?(Array)
+            @contents = {"message" => "200", "results" => @jsonContent}
+        else
+            @contents = @jsonContent
+        end
+    else
+      @contents = JSON.parse('{"message": "Please enter an owner and repo."}')
+    end
   end
 end
