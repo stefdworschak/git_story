a06bd396b7517979bb5a3b8d9fa068697fd6888d
stefdworschak <stef.dworschak@gmail.com>
2020-03-21 14:51:50 +0000

    creating admin user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin_user = User.new
+admin_user.email = 'admin@gmail.com'
+admin_user.password = 'Password1'
+admin_user.password_confirmation =
+admin_user.admin = true
+admin_user.save!
