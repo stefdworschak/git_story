aefbe5c7565fca10649c6ee2fbd9cee151d0a6ee
stefdworschak <stef.dworschak@gmail.com>
2020-03-21 21:28:29 +0000

    fixing basic_auth gem and changing GitHub

--- lib/github.rb
+++ lib/github.rb
@@ -1,10 +1,15 @@
 require 'basic_auth_http'
 class Github
-    @error = {"status" => "404", "message": "File not found"}
     def initialize
         @github_user = Rails.application.credentials.github_api_user
         @github_token = Rails.application.credentials.github_api_token
         BasicAuthHTTP.new(@github_user, @github_token)
+    end
+
+    def get_commits_for_file repo, filename
+      commits_url = repo[/(.*\/)contents/, 1] + 'commits?path=' + filename
+      contents = get_contents(commits_url)
+      puts contents
     end
 
     def greet
@@ -16,12 +21,15 @@ class Github
     end
 
     def get_contents url
-        preview_header = [{"field" => "Accept", "field_value" => "application/vnd.github.cloak-preview"}]
-        response = BasicAuthHTTP.get_request(url, preview_header)
-        if response.nil?
-            return @error
+        #preview_header = [{"field" => "Accept", "field_value" => "application/vnd.github.cloak-preview"}]
+        #response = BasicAuthHTTP.get_request(url, preview_header)
+        response = BasicAuthHTTP.get_request(url)
+        if response.nil? or response.code != 200
+            error = {"status" => response.code, "message" => response.message}
+            return error
         end
-        return response.body
+        contents = {"status" => response.code, "message" => response.body}
+        return contents
     end
 
 end # End Module
